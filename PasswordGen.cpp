0c3565c078da7807fe96deb7fe5d6218d78e34da
Nemanja Djordjic <50496679+ConeDjordjic@users.noreply.github.com>
2019-05-12 00:45:35 +0200

    Removed useless white spaces

--- PasswordGen.cpp
+++ PasswordGen.cpp
@@ -6,7 +6,6 @@ using namespace std;
 string generateRandomId(size_t length)
 {
     static const std::string allowed_chars {"123456789BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz~`!@#$%^&*()_-+=[]{};:,<.>/?"};
-
     static thread_local std::default_random_engine randomEngine(std::random_device{}());
     static thread_local std::uniform_int_distribution<int> randomDistribution(0, allowed_chars.size() - 1);
 
@@ -15,15 +14,12 @@ string generateRandomId(size_t length)
     for (std::string::value_type& c : id) {
         c = allowed_chars[randomDistribution(randomEngine)];
     }
-
     return id;
 }
 
 
 int main(){
-
 	size_t len,times;
-
 	cout << "Welcome to Random Password Generator by Cone!\n";
 	
 	cout << "Enter the length of the password(s): ";
@@ -35,7 +31,4 @@ int main(){
 	for (int i = 0; i < times; i++){
 		cout << generateRandomId(len) << "\n";	
 	}
-
-
-
 }
